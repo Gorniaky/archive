3d63901c7488ac737bce84fd3a50a0959bc6eaa2
Gorniaky <30408913+Gorniaky@users.noreply.github.com>
2021-06-20 11:50:27 -0300

    Update Clean - Defrag.cmd

--- Clean - Defrag.cmd
+++ Clean - Defrag.cmd
@@ -8,13 +8,13 @@ if exist "%temp%\getadmin.vbs" (del "%temp%\getadmin.vbs")
 exit /B
 :gotAdmin
 echo Start %time% %date%>>CleanDefrag.log
->nul 2>nul (del /f /s /q %temp%\*.*
-del /f /s /q %windir%\temp\*.*
-del /f /s /q %windir%\SoftwareDistribution\Download\*.*
-del /f /s /q %programdata%\temp\*.*
-del /f /s /q %systemdrive%\$Recycle.Bin\*.*
-del /f /s /q %appdata%\Adobe\Common\"Media Cache Files"\*.*
-del /f /s /q %localappdata%\Packages\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe\TempState\WinGet)
+>nul 2>nul (for %%a in ("%temp%"
+"%windir%\temp"
+"%windir%\SoftwareDistribution\Download"
+"%programdata%\temp"
+"%systemdrive%\$Recycle.Bin"
+"%appdata%\Adobe\Common\Media Cache Files"
+"%localappdata%\Packages\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe\TempState\WinGet") do if exist %%a (forfiles /p %%a /c "cmd /c if @isdir==TRUE (rd /s /q @file) else (del /f /s /q @file)"))
 echo Clean %time% %date%>>CleanDefrag.log
 for %%a in (a b c d e f g h i j k l m n o p q r s t u v w x y z) do if exist %%a: (cls&defrag %%a: /u /v)
 echo Finish defrag %time% %date%>>CleanDefrag.log
