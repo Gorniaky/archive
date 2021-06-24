f2f9f8130ca3c92d1fa32fd4135b4392428061f4
Gorniaky <30408913+Gorniaky@users.noreply.github.com>
2021-06-23 22:03:39 -0300

    Update Clean - Defrag.cmd

--- Clean - Defrag.cmd
+++ Clean - Defrag.cmd
@@ -2,7 +2,7 @@
 goto:start
 
 :searchpath
-for %%b in (Windows.old,Windows10Upgrade) do %nul% forfiles /m %%b* /p %drivepath%\ /c "cmd /c echo @path>>%temp%\erasepaths.txt"
+for %%b in ($Recycle.Bin,Windows.old,Windows10Upgrade) do %nul% forfiles /m %%b* /p %drivepath%\ /c "cmd /c echo @path>>%temp%\erasepaths.txt"
 exit/b
 
 :erasepath
@@ -10,10 +10,19 @@ for /f delims^=^" %%c in (%temp%\erasepaths.txt) do set erasepath=%%c&call:erase
 exit/b
 
 :erase
+set noerase=
+for %%d in ($Recycle.Bin) do echo "%erasepath%"|%nul% findstr /i %%d && set noerase=1
+if not defined noerase (
 %nul% takeown /f "%erasepath%" /r /d N
 %nul% icacls "%erasepath%" /grant *S-1-1-0:F /t /c /q
 %nul% rd /s /q "%erasepath%"
 if exist "%erasepath%" (echo Fail to erase "%erasepath%") else (echo "%erasepath%" erased)
+)
+%nul% del /f /s /q "%erasepath%"\*.*
+exit/b
+
+:setdrivelist
+set drivelist=%drivelist%%drive% 
 exit/b
 
 :start
@@ -27,17 +36,18 @@ if exist "%temp%\getadmin.vbs" (del "%temp%\getadmin.vbs")
 exit /B
 :gotAdmin
 echo Start %time% %date%>>CleanDefrag.log
+for %%a in (A B C D E F G H I J K L M N O P Q R S T U V W X Y Z) do if exist %%a:\ (set drive=%%a&call:setdrivelist)
 if exist "%temp%\erasepaths.txt" (del "%temp%\erasepaths.txt")
-for %%a in (A B C D E F G H I J K L M N O P Q R S T U V W X Y Z) do if exist %%a:\ (set drivepath=%%a:&call:searchpath)
+for %%a in (%drivelist%) do if exist %%a:\ (set drivepath=%%a:&call:searchpath)
 if exist "%temp%\erasepaths.txt" call:erasepath
+
 %nul% (for %%a in ("%temp%"
 "%windir%\temp"
 "%windir%\SoftwareDistribution\Download"
 "%programdata%\temp"
-"%systemdrive%\$Recycle.Bin"
 "%appdata%\Adobe\Common\Media Cache Files"
 "%localappdata%\Packages\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe\TempState\WinGet") do if exist %%a (forfiles /p %%a /c "cmd /c if @isdir==TRUE (rd /s /q @file) else (del /f /s /q @file)"))
 echo Clean %time% %date%>>CleanDefrag.log
-pause
-for %%a in (A B C D E F G H I J K L M N O P Q R S T U V W X Y Z) do if exist %%a: (cls&defrag %%a: /u /v)
+
+for %%a in (%drivelist%) do if exist %%a:\ (cls&defrag %%a:)
 echo Finish defrag %time% %date%>>CleanDefrag.log
